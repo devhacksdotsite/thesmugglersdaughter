68755918762b4662fdc342c04918151619e66045
Jesse Salinas <salinas.webdev@gmail.com>
2026-07-04 09:18:04 -0700

    update: Switch site font to Manrope

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,26 +13,11 @@ export default defineConfig({
 
   fonts: [
       {
-          provider: fontProviders.local(),
-          name: 'Atkinson',
-          cssVariable: '--font-atkinson',
+          provider: fontProviders.google(),
+          name: 'Manrope',
+          cssVariable: '--font-manrope',
           fallbacks: ['sans-serif'],
-          options: {
-              variants: [
-                  {
-                      src: ['./src/assets/fonts/atkinson-regular.woff'],
-                      weight: 400,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-                  {
-                      src: ['./src/assets/fonts/atkinson-bold.woff'],
-                      weight: 700,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-              ],
-          },
+          weights: [400, 500, 600, 700, 800],
       },
 	],
 
